a24621dc22e4895a6c6705ae8bb6bbb2913a16ab
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-03-10 18:44:31 +0000

    Lecture 7 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import Button from "./components/Button"
 function App() {
 
   return (
@@ -8,6 +8,7 @@ function App() {
         <h1 className='text-xl font-bold underline mb-5 mt-6'>Hi this is</h1>
         <p className="text-lg font-semibold text-sky-500">Mubassim Ahmed Khan</p>
       </div>
+      <Button text="nothing" />
     </>
   )
 }
