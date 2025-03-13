265173ce309ccc81643787414377f355a78d6678
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-03-13 16:47:57 +0000

    Feat: Add route for POST

--- backend/routes/projects.ts
+++ backend/routes/projects.ts
@@ -1,16 +1,42 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { projects } from "../projects";
 
 const router = express.Router();
 
 // GET - Fetch all projects
-router.get("/projects", (req, res) => {
+router.get("/projects", (req: Request, res: Response) => {
   res.json({ status: "success", projects });
 });
 
 // POST - Add new project
-router.get("/projects", (req, res) => {
-  res.json({ status: "success", projects });
+router.post("/projects", (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  if (!name || name.length < 3) {
+    res.status(400).json({
+      status: "error",
+      message: "Project name must be at least 3 characters long",
+    });
+    return;
+  }
+
+  try {
+    const newProject = { id: projects.length + 1, name: req.body.name };
+    projects.push(newProject);
+
+    res.status(201).json({
+      status: "success",
+      message: "Project add successfully",
+      project: newProject,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      error: error.message,
+      message: "Something went wrong.",
+    });
+  }
 });
 
 module.exports = router;
