2792aaeb86ce21517f5f8701dc84510cf2220c8b
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-03-13 17:19:40 +0000

    Feat: Add route for PUT

--- backend/routes/projects.ts
+++ backend/routes/projects.ts
@@ -25,13 +25,13 @@ router.post("/projects", (req: Request, res: Response) => {
     const newProject = { id: projects.length + 1, name: req.body.name };
     projects.push(newProject);
     saveProjects(projects);
-    
+
     res.status(201).json({
       status: "success",
-      message: "Project add successfully",
+      message: "Project added successfully",
       project: newProject,
     });
-  } catch (error) {
+  } catch (error: any) {
     console.error(error);
     res.status(500).json({
       status: "error",
@@ -40,5 +40,57 @@ router.post("/projects", (req: Request, res: Response) => {
     });
   }
 });
+
+// PUT - Update exsisting project
+router.put("/projects/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!id) {
+    res.status(400).json({
+      status: "error",
+      message: "Must include ID",
+    });
+    return;
+  }
+
+  if (!name || name.length < 3) {
+    res.status(400).json({
+      status: "error",
+      message: "Project name must be at least 3 characters long",
+    });
+    return;
+  }
+
+  try {
+    const projectIndex = projects.findIndex((p) => p.id === parseInt(id));
+
+    if (projectIndex === -1) {
+      res.status(404).json({
+        status: "error",
+        message: "Project not found",
+      });
+      return;
+    }
+
+    projects[projectIndex].name = name;
+    saveProjects(projects);
+
+    res.status(200).json({
+      status: "success",
+      message: "Project updated successfully",
+      project: projects[projectIndex],
+    });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      error: error.message,
+      message: "Something went wrong.",
+    });
+  }
+});
+
+// DELETE - Delete a project
 
 module.exports = router;
