0e4ceb86be09ef0bd1e960fa37413c550d4274cb
Mubassim Ahmed Khan <113042537+Mubassim-Khan@users.noreply.github.com>
2025-03-13 18:07:09 +0000

    Add necessary packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Button from "./components/Button"
+import { ToasterProvider } from './components/ToasterProvider'
 function App() {
 
   return (
     <>
-      <div>
+      <ToasterProvider />
         <h1 className='text-xl font-bold underline mb-5 mt-6'>Hi this is</h1>
         <p className="text-lg font-semibold text-sky-500">Mubassim Ahmed Khan</p>
-      </div>
       <Button text="nothing" />
     </>
   )
